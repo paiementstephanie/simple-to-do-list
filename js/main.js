19e351b0d2386119ed30524a1e0bac1e6280c159
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-15 17:00:12 -0400

    added css line through

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var $input = $('#simple-input');
 var $list = $('.list');
+var $form = $('form');
 
 $('#simple-list').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
-  $li.html($input.val());
+  $li.html($input.val(''));
   $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
 });
